f5ad39e98744caad301e78bc758dafb9ebdffe2c
uglysquid49 <mrg4309@autuni.ac.nz>
2026-07-15 17:41:09 +1200

    scorescript

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class PointManager : MonoBehaviour
+{
+
+    public int score;
+    public TMP_Text scoreText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+   public void UpDateScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+    }
+}

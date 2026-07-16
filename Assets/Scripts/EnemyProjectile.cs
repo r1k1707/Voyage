6bd97c1ca3cde8e7d60b48fa03de9ccd13565c38
r1k1 <yu.izanagi.persona4@pm.me>
2026-07-16 16:55:00 +1200

    adasdad

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public float speed;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+}

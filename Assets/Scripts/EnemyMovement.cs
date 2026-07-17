953f19f00eec6bd31a90a18c9f1b50793ad4e183
aslesmv <aslesmv@gmail.com>
2026-07-17 13:17:16 +1200

    17/07/2026 Update
    Enemy flies down towards player and shoots as well

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // for the enemy speed
+    public float speed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // finding the enemy prefabs current position
+        Vector2 position = transform.position;
+
+        // Calculating the new position of enemies
+        position = new Vector2 (position.x, position.y - speed * Time.deltaTime);
+
+        // line to update the enemy position
+        transform.position = position;
+
+        // this refers to the bottom-left point of the screen
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+
+        // if enemy leaves screen from bottom of screen, then destroy object
+        if (transform.position.y < min.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

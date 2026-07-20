fb848693141348a5d2154c03266dfa3df8247fab
aslesmv <aslesmv@gmail.com>
2026-07-20 15:14:50 +1200

    hdsgfjhsgdf

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -8,7 +8,16 @@ public class TimerController : MonoBehaviour
     
     void Update()
     {
-        remainingTime -= Time.deltaTime;
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);

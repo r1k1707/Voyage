cd133f435d8e2f94f5a3f0df577d4c40d38d82a6
uglysquid49 <mrg4309@autuni.ac.nz>
2026-07-21 20:52:38 +1200

    shibal

--- Assets/Scripts/LevelClear.cs
+++ Assets/Scripts/LevelClear.cs
@@ -1,28 +1,55 @@
 using UnityEngine;
+using TMPro;
 
 public class LevelClear : MonoBehaviour
 {
     public GameObject levelClear;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_Text finalScoreText;
+    public TMP_Text highScoreText;
+    public TMP_Text finalTimeText;
+
+    private PointManager pointManager;
+    private TimerController timerController;
+
     void Start()
     {
         levelClear.SetActive(false);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Find the managers in the scene
+        pointManager = FindFirstObjectByType<PointManager>();
+        timerController = FindFirstObjectByType<TimerController>();
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             levelClear.SetActive(true);
+
+            ShowResults();
+
             Time.timeScale = 0f;
+
             Debug.Log("about time bro");
+        }
+    }
+
+    void ShowResults()
+    {
+        // Final score
+        if (pointManager != null)
+        {
+            finalScoreText.text = pointManager.score.ToString();
+        }
+
+        // High score
+        highScoreText.text = PlayerPrefs.GetInt("SavedHighScore").ToString();
+
+        // Final time
+        if (timerController != null)
+        {
+            finalTimeText.text = timerController.timerText.text;
         }
     }
 }
--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -3,25 +3,45 @@ using TMPro;
 
 public class TimerController : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI timerText;
+    public TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
     public GameOverManager gameOverManager;
 
+    //pull playerLives script
+    private PlayerLives playerLives;
+
+    public TMP_Text finalTimeText;
+
+    void Start()
+    {
+        playerLives = FindFirstObjectByType<PlayerLives>();
+    }
     void Update()
     {
         if (remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
+            // Prevents the timer going below 0
+            if (remainingTime < 0)
+            {
+                remainingTime = 0;
+            }
         }
-
-        else if (remainingTime < 0)
+        else
         {
             remainingTime = 0;
-            gameOverManager.GameOver();
+            if (playerLives != null)
+            {
+                playerLives.TimerDeath();
+            }
         }
 
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    public void ShowFinalTime()
+    {
+        finalTimeText.text = timerText.text;
     }
 }

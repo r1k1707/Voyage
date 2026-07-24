4688eef3791129bac12c5a0fc703366eff938db5
r1k1 <yu.izanagi.persona4@pm.me>
2026-07-25 01:07:07 +1200

    asdasda

--- Assets/Scripts/BulletShoot.cs
+++ Assets/Scripts/BulletShoot.cs
@@ -6,18 +6,16 @@ public class BulletShoot : MonoBehaviour
 {
     public GameObject bulletsPrefab;
     public float time = 1.0f;
+    public float fireRate = 0.75f;   // Time between shots
     public float timer = 0;
 
     private void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= time)
+        if (Input.GetButton("Fire1") && timer >= fireRate)
         {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                shootBullet();
-                timer = 0;
-            }
+            shootBullet();
+            timer = 0f;
         }
     }
 
